b7a60b905ab7ac7ee43b77b523fa0f87b04702fd
Nick Hopewell <nicholashopewell@gmail.com>
2020-08-31 12:58:41 -0400

    adds initial files

---
+++ django_project/blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render # looks in templated folder
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Nick',
+        'author_status': 'admin',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Aug 30, 2020'
+    },
+    {
+        'author': 'Jim',
+        'author_status': 'user',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Aug 31, 2020'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
