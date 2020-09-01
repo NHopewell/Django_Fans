4cf9a763eb8755687ba5b011ca133ddd260d9865
Nick Hopewell <nicholashopewell@gmail.com>
2020-08-31 22:06:57 -0400

    updates base temp

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    # create a registration form with django forms
-    form = UserCreationForm()
+
+    if request.method == 'POST':
+        # create a form with data entered by user
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.clean_data.get("username") #clean_data dict
+            messages.success(request, f'Account created for {username}.')
+            
+            return redirect('blog-home')
+    else:
+        # create a blank registration form with django forms
+        form = UserCreationForm()
 
     return render(request, 'users/register.html', {'form': form})
 
