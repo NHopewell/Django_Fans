550f5eca6174ba62c1c89ee2c90336952384daaf
Nick Hopewell <nicholashopewell@gmail.com>
2020-09-07 14:40:24 -0400

    handles login view authentication

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -21,6 +21,7 @@ from users import views as user_views # user views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name='login'), #tells django to look in users path
     path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name='logout'), #look in users
     # using blog app as homepage ''
