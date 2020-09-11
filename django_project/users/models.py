1d735f90febc96bcab5bee7519cbe8b498576fcf
Nick Hopewell <nicholashopewell@gmail.com>
2020-09-10 20:31:12 -0400

    updates media root and media url

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -7,7 +7,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     # one to one relationship with the user mode
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics') # save pics in this dir
+    # save pics in this dir, extends MEDIA_DIR in settings.py
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics') 
 
     def __str__(self):
         return f"{self.user.username} Profile"
