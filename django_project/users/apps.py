31e895e16004562ac242a49c1c022db73db616bf
Nick Hopewell <nicholashopewell@gmail.com>
2020-09-19 19:45:13 -0400

    adds user signals for create and save profile

--- django_project/users/apps.py
+++ django_project/users/apps.py
@@ -3,3 +3,10 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'users'
+
+    def ready(self):
+        """
+        django docs recommend importing this way to
+        avoid side effects of importing'
+        """
+        import users.signals
--- django_project/users/signals.py
+++ django_project/users/signals.py
@@ -8,7 +8,27 @@ from .models import Profile
 Create a new profile for each user.
 post_save sends signal from User (sender)
 to reciever (django.dispatch)
+Receiver is a function that gets the signal
+and performs some task
 """
 
-def create_profile(sender, profile, created, **kwargs):
-    pass
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    """
+    receiver function that runs every time a user is created.
+    signal = post_save
+    sender = User
+    instance = user instance
+    created = was the user created?
+    """
+    # if that user was created, create a profile for that user
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    """
+    receiver function that runs every time a user is saved.
+    """
+    instance.profile.save()
+
