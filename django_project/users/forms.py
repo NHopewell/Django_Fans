dd8f86767b68d82ae4cfd8eb7a6771ff169bf3b8
Nick Hopewell <nicholashopewell@gmail.com>
2020-09-19 19:57:33 -0400

    adds user and profile update forms

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 # 
 class UserRegisterForm(UserCreationForm):
@@ -8,7 +9,7 @@ class UserRegisterForm(UserCreationForm):
     create user registration form which inherits 
     from UserCreationForm and adds an email field.
     """
-
+    # specify additional feilds to add to form
     email = forms.EmailField(required=True)
     
     class Meta:
@@ -17,3 +18,25 @@ class UserRegisterForm(UserCreationForm):
         model = User
         # fields we want to show on form (in order):
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    """
+    Model form to update our user model,
+    required before user can update her/his
+    profile on the front end.
+    """
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        # allow user to update their username and email
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    """
+    Model form to allow user to update their
+    profile (with a new profile pic)
+    """
+    class Meta:
+        model = Profile
+        fields = ['image']
