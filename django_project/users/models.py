c41676e3ce34836f4a1ab9dd87177f00761d4b83
Nick Hopewell <nicholashopewell@gmail.com>
2020-09-22 08:47:40 -0400

    updates default profile pic

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     # one to one relationship with the user mode
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # save pics in this dir, extends MEDIA_DIR in settings.py
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics') 
+    image = models.ImageField(default='default.png', upload_to='profile_pics') 
 
     def __str__(self):
         return f"{self.user.username} Profile"
