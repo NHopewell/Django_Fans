876e953540b9bd47ce5dd2dfce50cef00efd69f2
Nick Hopewell <nicholashopewell@gmail.com>
2020-09-22 08:43:47 -0400

    updates save to resize img

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -12,3 +13,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
+
+    def save(self, *args, **kwargs):
+        """override save() to resize img to save
+           room on file system"""
+        super().save()
+
+        # resive img if largerß
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img = img.convert("RGB")
+            img.save(self.image.path) # save to same path
