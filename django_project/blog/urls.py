79a6e4fbb161f7fb999cdda60321797e0984c05b
Nick Hopewell <nicholashopewell@gmail.com>
2020-09-23 08:23:49 -0400

    adds post details view

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 # views from views.py
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
     # home route (blog/)
-    path('', views.home, name="blog-home"),
+    path('', PostListView.as_view(), name="blog-home"),
+    # post detail route, pk = primary key
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     # about route (blog/about/)
     path('about/', views.about, name="blog-about"),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render # looks in templated folder
+from django.shortcuts import render # looks in templates folder
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 def home(request):
@@ -6,6 +7,23 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    """
+    Home route: blog/home.html
+    ListView: contains list of Post objects
+    """
+    model = Post
+    template_name = 'blog/home.html' #Default convention: <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] # order posts ascending
+
+class PostDetailView(DetailView):
+    """
+    Post Detail route: blog/post_detail.html route
+    """
+    model = Post
+    context_object_name = 'post'
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
