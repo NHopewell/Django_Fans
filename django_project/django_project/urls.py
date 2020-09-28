cd5317899a87660c782e1d96b2e15930d0da9148
Nick Hopewell <nicholashopewell@gmail.com>
2020-09-27 20:48:28 -0400

    adds password reset and reset done routing

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -22,10 +22,29 @@ from users import views as user_views # user views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('register/', user_views.register, name='register'),
+
     path('profile/', user_views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name='login'), #tells django to look in users path
-    path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name='logout'), #look in users
+
+    path('login/', auth_views.LoginView.as_view(
+            template_name="usÍers/login.html"), 
+        name='login'), #tells django to look in users path
+
+    path('logout/', auth_views.LogoutView.as_view(
+            template_name="users/logout.html"), 
+        name='logout'), #look in users
+
+    path('password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name="users/password_reset.html"), 
+        name='password_reset'),
+
+    path('password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="users/password_reset_done.html"), 
+        name='password_reset_done'),
+
     # using blog app as homepage ''
     path('', include('blog.urls')),
 ]
